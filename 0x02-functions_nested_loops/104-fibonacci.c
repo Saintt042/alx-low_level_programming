412c64da67b3b82d76c38dbb75036e9aadd70c6f
Alinwachukwu Michael <saintmichaell42@gmail.com>
2023-08-16 00:58:04 +0100

    Advanced

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -4,28 +4,34 @@
  *
  * Return: nothing
  */
+void print_fibonacci(int n);
+
 int main(void)
 {
-	int counter = 2;
+    print_fibonacci(98);
+    printf("\n");
 
-	float a = 1;
-	float b = a + 1;
-	float c = a + b;
+    return 0;
+}
 
-	printf("%.0f, ", a);
-	printf("%.0f, ", b);
-	while (counter < 98)
-	{
-		counter++;
-		printf("%.0f", c);
-		a = b;
-		b = c;
-		c = a + b;
-		if (counter < 98)
-		{
-			printf(", ");
-		}
-	}
-	printf("\n");
-	return (0);
+void print_fibonacci(int n)
+{
+    unsigned long int a = 1, b = 2, next_term;
+
+    if (n >= 1)
+        printf("%lu, ", a);
+    if (n >= 2)
+        printf("%lu, ", b);
+
+    for (int i = 3; i <= n; i++)
+    {
+        next_term = a + b;
+        if (i < n)
+            printf("%lu, ", next_term);
+        else
+            printf("%lu", next_term);
+
+        a = b;
+        b = next_term;
+    }
 }
